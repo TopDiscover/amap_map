cd9db0eead505e4d41fd783980b9f5262d1832b2
zhengfasheng <zheng_fasheng@outlook.com>
2025-03-31 10:48:37 +0800

    换自己的android Key

--- example/lib/const_config.dart
+++ example/lib/const_config.dart
@@ -2,7 +2,7 @@ import 'package:x_amap_base/x_amap_base.dart';
 
 class ConstConfig {
   static const AMapApiKey amapApiKeys = AMapApiKey(
-      androidKey: '900f72eeee0f21e435cebb0ef155582a',
+      androidKey: 'd7eef7eabad2ecc7d2ab31c56f6bf9cc',
       iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
   static const AMapPrivacyStatement amapPrivacyStatement =
       AMapPrivacyStatement(hasContains: true, hasShow: true, hasAgree: true);
